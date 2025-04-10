2a25c016babe2356a5d1c95c07c45020ae17fabc
ShawnSomething <khooweishawn@gmail.com>
2025-04-11 06:24:50 +1000

    start screen

--- chicken-heist/src/comp/start-screen.tsx
+++ chicken-heist/src/comp/start-screen.tsx
@@ -8,14 +8,16 @@ interface StartScreenProps {
 
 export const StartScreen: React.FC<StartScreenProps> = ({ onStart }) => {
     return (
-        <div className="fixed h-screen w-screen">
+        <>
+        <div className="fixed h-screen w-screen overflow-hidden">
             <img src={titleCard} className="w-full h-full object-fill" />
-            <button
-                onClick={onStart}
-                className="fixed inset-x-10 bottom-3 flex justify-center scale-75 hover:scale-100 transition-transform"
-            >
-                <img src={startButton} className="w-50 h-24" />
-            </button>
         </div>
+        <button
+            onClick={onStart}
+            className="fixed bottom-[-60px] left-1/2 transform -translate-x-1/2 w-52 h-52 scale-50 hover:scale-75 transition-transform"
+        >
+            <img src={startButton} />
+            </button>
+            </>
     );
 };
