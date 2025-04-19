58f2b66abb1e1127810b4a97ee05d001c10bb7c1
ShawnSomething <khooweishawn@gmail.com>
2025-04-19 11:24:53 +1000

    transparent background videos

--- chicken-heist/src/comp/body.tsx
+++ chicken-heist/src/comp/body.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import comm from './comm device.mp4'
+import commT from './commT.mp40001-0025.webm'
 
 export const Body = () => {
     return(
         <>
             <div className='text-3xl flex justify-center'>
-                <video autoPlay loop src={comm} className="p-28 top-0"></video>
+                <video autoPlay loop playsInline src={commT} style={{backgroundColor: 'transparent'}} className="p-28 top-0"></video>
             </div>
         </>
     )
--- chicken-heist/src/custom.d.ts
+++ chicken-heist/src/custom.d.ts
@@ -8,3 +8,8 @@ declare module "*.mp4" {
     const value: string
     export default value
 }
+
+declare module "*.webm" {
+    const value: string
+    export default value
+}
